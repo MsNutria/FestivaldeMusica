76513a0ce958ee5919d042811daf3d01f0d5eff8
Margi Africano <africanomargi@gmail.com>
2022-05-30 19:29:21 -0500

    Mejorando codigo css y js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
-const { src, dest, watch, parallel} = require("gulp");
+const { src, dest, watch, parallel } = require('gulp');
 
-//CSS
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
-//Imagenes
+// Imagenes
 const cache = require('gulp-cache');
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 const avif = require('gulp-avif');
+
+//Javascript
+const terser = require('gulp-terser-js');
 
 function css(done) {
     src("src/scss/**/*.scss") // Identificar el archivo SASS
-        .pipe(plumber())
-        .pipe(sass()) // Compilarlo
-        .pipe(dest("build/css")) //Almacenarlo en el disco duro
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe( sass() ) // Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('build/css') ) //  //Almacenarlo en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final    
 }
@@ -51,6 +61,9 @@ function versionAvif( done ) {
 
 function javascript( done ) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
